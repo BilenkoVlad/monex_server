134df8c837a6a66953c8a8ffc1992665c889d7f4
BilenkoVlad <Vladik99!!!>
2023-10-15 14:33:17 +0200

    cache is added

--- api/mono_bank_api.py
+++ api/mono_bank_api.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+import requests_cache
 
 
 class MonoBankApi:
@@ -9,4 +10,5 @@ class MonoBankApi:
         self._base_api_url = os.environ.get('MONO_BANK_URL')
 
     def uah_rates(self):
+        requests_cache.install_cache('mono_cache', expire_after=300)
         return requests.get(url=f"{self._base_api_url}").json()
