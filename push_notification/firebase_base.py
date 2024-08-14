7601a5a405f4818eb582a6055792f51802c09bec
BilenkoVlad <bilenko.vladyslav.99@gmail.com>
2024-08-14 16:23:50 +0200

    try new

--- push_notification/firebase_base.py
+++ push_notification/firebase_base.py
@@ -13,20 +13,19 @@ class FirebaseBase:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls._instance = super(FirebaseBase, cls).__new__(cls, *args, **kwargs)
+            remote = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+            service_account_file = f'{os.path.dirname(os.path.abspath(__file__))}/secret.json'
+
+            if remote is not None:
+                with open(service_account_file, 'w') as json_file:
+                    json.dump(json.loads(remote), json_file, indent=4)
+
+            creds = credentials.Certificate(service_account_file)
+            firebase_admin.initialize_app(creds)
         return cls._instance
 
     def __init__(self):
-        remote = os.getenv("FIREBASE_SERVICE_ACCOUNT")
-        service_account_file = f'{os.path.dirname(os.path.abspath(__file__))}/secret.json'
-
-        if remote is not None:
-            with open(service_account_file, 'w') as json_file:
-                json.dump(json.loads(remote), json_file, indent=4)
-
-        self.creds = credentials.Certificate(service_account_file)
-        firebase_admin.initialize_app(self.creds)
-
         self.database = firestore.client()
         self.server_url = os.environ.get('SERVER_URL')
         self.api_data = "api_data"
